1a5520847c67cd9b1c9e1ae06006ed75c429e301
Simone Baravelli <simobara@hotmail.it>
2023-12-12 14:06:19 +0100

    analizzo struttura commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Modifica questo percorso in base alla tua struttura di progetto
+    // Puoi aggiungere altri percorsi di file qui se necessario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
